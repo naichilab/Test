02c571e4298420c93f31e7e7e917621cd644c2b7
kassy916 <warallyinfo@gmail.com>
2018-02-22 15:38:36 +0000

    first commit

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -32,7 +32,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
   # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
+  # cancel oauth signingdevise_parameter_sanitizer in/up in the middle of the process,
   # removing all OAuth session data.
   # def cancel
   #   super
@@ -47,7 +47,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  #   .permit(:account_update, keys: [:attribute])
   # end
 
   # The path used after sign up.
--- app/models/pending.rb
+++ app/models/pending.rb
@@ -1,2 +1,7 @@
 class Pending < ApplicationRecord
+	belongs_to :user
+	belongs_to :event
+
+	#1ユーザー1回まで
+	validates_uniqueness_of :live_id, scope: :uid
 end
